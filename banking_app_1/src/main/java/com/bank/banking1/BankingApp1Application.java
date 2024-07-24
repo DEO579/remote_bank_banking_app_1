fd0780a743b74ef7ac0c931ffffcab2c2b9781d0
DEO579 <133693744+DEO579@users.noreply.github.com>
2024-07-24 13:07:17 +0530

    Add files via upload

---
+++ banking_app_1/src/main/java/com/bank/banking1/BankingApp1Application.java
@@ -0,0 +1,13 @@
+package com.bank.banking1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingApp1Application.class, args);
+	}
+
+}
