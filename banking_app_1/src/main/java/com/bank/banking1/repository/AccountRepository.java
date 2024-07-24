1d9ded7a0cf89b41eacc165a53ce1b9e171f93c1
DEO579 <deo.kr59998@gmail.com>
2024-07-24 14:28:12 +0530

    create database

---
+++ banking_app_1/src/main/java/com/bank/banking1/repository/AccountRepository.java
@@ -0,0 +1,7 @@
+package com.bank.banking1.repository;
+
+import com.bank.banking1.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
